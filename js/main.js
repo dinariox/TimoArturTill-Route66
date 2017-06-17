858e94640b9d87cd414bf8d3151cef95bb2b5903
dinariox <timo.n.2000.de@gmail.com>
2017-06-17 19:10:10 +0200

    Die komplette Startseite ist fertig
    Materialize verwendet

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+// JavaScript & jquery © 2017 by Timo Nowak, Artur Makarenko & Till Hammer
+
+
+// Wenn der Menü Button geklickt wird,
+// soll das mobile Menü aufgeklappt oder
+// wieder geschlossen werden
 
 var menuVisibleState = false;
 
@@ -16,3 +22,14 @@ $('#menu-button').click(function() {
     }
 
 })
+
+
+// Wenn der Bildschirm kleiner als 600px ist,
+// soll das Logo ohne Text angezeigt werden,
+// um Überscheidung mit dem Menü-Button oder
+// dem rechten Bilschirmrand zu verhindern
+if($(window).width() <= 600) {
+
+    $('#logo').attr("src", "../img/logo.png");
+    
+}
